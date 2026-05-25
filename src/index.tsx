ef5289fb56f6a9d153c188e39e540467a6641985
Rishabh Yadav <rrishuyadav@gmail.com>
2026-05-25 14:58:11 +0200

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,47 @@
 import { Platform } from 'react-native';
 
-let Encryption: any;
+export interface KeyPair {
+  publicKey: string;
+  privateKey: string;
+}
+
+type EncryptionModule = {
+  generateAESKey(input: number): string;
+  encryptAES(data: string, key: string): string;
+  decryptAES(data: string, key: string): string;
+  encryptRSA(data: string, key: string): string;
+  decryptRSA(data: string, key: string): string;
+  generateRSAKeyPair(): KeyPair;
+  generateHMACKey(keySize: number): string;
+  hmacSHA256(data: string, key: string): string;
+  hmacSHA512(data: string, key: string): string;
+  hashSHA256(input: string): string;
+  hashSHA512(input: string): string;
+  generateRandomString(input: number): string;
+  base64Encode(input: string): string;
+  base64Decode(input: string): string;
+  generateECDSAKeyPair(): KeyPair;
+  signDataECDSA(data: string, key: string): string;
+  verifySignatureECDSA(
+    data: string,
+    signatureBase64: string,
+    key: string
+  ): boolean;
+  encryptAsyncAES(data: string, key: string): Promise<string>;
+  decryptAsyncAES(data: string, key: string): Promise<string>;
+  encryptAsyncRSA(data: string, key: string): Promise<string>;
+  decryptAsyncRSA(data: string, key: string): Promise<string>;
+  encryptFile(
+    inputPath: string,
+    outputPath: string,
+    key: string
+  ): Promise<string>;
+  decryptFile(inputPath: string, key: string): Promise<string>;
+  getPublicRSAkey(privateRSAkey: string): string;
+  getPublicECDSAKey(privateECDAkey: string): string;
+};
+
+let Encryption: EncryptionModule;
 
 if (Platform.OS === 'web') {
   // Use web-encryption for web
@@ -10,31 +51,110 @@ if (Platform.OS === 'web') {
   Encryption = require('./native/index');
 }
 
-// Export all encryption methods
-export const {
-  generateAESKey,
-  encryptAES,
-  decryptAES,
-  encryptRSA,
-  decryptRSA,
-  generateRSAKeyPair,
-  generateHMACKey,
-  hmacSHA256,
-  hmacSHA512,
-  hashSHA256,
-  hashSHA512,
-  generateRandomString,
-  base64Encode,
-  base64Decode,
-  generateECDSAKeyPair,
-  signDataECDSA,
-  verifySignatureECDSA,
-  encryptAsyncAES,
-  decryptAsyncAES,
-  encryptAsyncRSA,
-  decryptAsyncRSA,
-  encryptFile,
-  decryptFile,
-  getPublicRSAkey,
-  getPublicECDSAKey,
-} = Encryption;
+export function generateAESKey(input: number): string {
+  return Encryption.generateAESKey(input);
+}
+
+export function encryptAES(data: string, key: string): string {
+  return Encryption.encryptAES(data, key);
+}
+
+export function decryptAES(data: string, key: string): string {
+  return Encryption.decryptAES(data, key);
+}
+
+export function encryptRSA(data: string, key: string): string {
+  return Encryption.encryptRSA(data, key);
+}
+
+export function decryptRSA(data: string, key: string): string {
+  return Encryption.decryptRSA(data, key);
+}
+
+export function generateRSAKeyPair(): KeyPair {
+  return Encryption.generateRSAKeyPair();
+}
+
+export function generateHMACKey(keySize: number): string {
+  return Encryption.generateHMACKey(keySize);
+}
+
+export function hmacSHA256(data: string, key: string): string {
+  return Encryption.hmacSHA256(data, key);
+}
+
+export function hmacSHA512(data: string, key: string): string {
+  return Encryption.hmacSHA512(data, key);
+}
+
+export function hashSHA256(input: string): string {
+  return Encryption.hashSHA256(input);
+}
+
+export function hashSHA512(input: string): string {
+  return Encryption.hashSHA512(input);
+}
+
+export function generateRandomString(input: number): string {
+  return Encryption.generateRandomString(input);
+}
+
+export function base64Encode(input: string): string {
+  return Encryption.base64Encode(input);
+}
+
+export function base64Decode(input: string): string {
+  return Encryption.base64Decode(input);
+}
+
+export function generateECDSAKeyPair(): KeyPair {
+  return Encryption.generateECDSAKeyPair();
+}
+
+export function signDataECDSA(data: string, key: string): string {
+  return Encryption.signDataECDSA(data, key);
+}
+
+export function verifySignatureECDSA(
+  data: string,
+  signatureBase64: string,
+  key: string
+): boolean {
+  return Encryption.verifySignatureECDSA(data, signatureBase64, key);
+}
+
+export function encryptAsyncAES(data: string, key: string): Promise<string> {
+  return Encryption.encryptAsyncAES(data, key);
+}
+
+export function decryptAsyncAES(data: string, key: string): Promise<string> {
+  return Encryption.decryptAsyncAES(data, key);
+}
+
+export function encryptAsyncRSA(data: string, key: string): Promise<string> {
+  return Encryption.encryptAsyncRSA(data, key);
+}
+
+export function decryptAsyncRSA(data: string, key: string): Promise<string> {
+  return Encryption.decryptAsyncRSA(data, key);
+}
+
+export function encryptFile(
+  inputPath: string,
+  outputPath: string,
+  key: string
+): Promise<string> {
+  return Encryption.encryptFile(inputPath, outputPath, key);
+}
+
+export function decryptFile(inputPath: string, key: string): Promise<string> {
+  return Encryption.decryptFile(inputPath, key);
+}
+
+export function getPublicRSAkey(privateRSAkey: string): string {
+  return Encryption.getPublicRSAkey(privateRSAkey);
+}
+
+export function getPublicECDSAKey(privateECDAkey: string): string {
+  return Encryption.getPublicECDSAKey(privateECDAkey);
+}
